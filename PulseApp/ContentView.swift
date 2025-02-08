e18085c59fadd0996322c7910f69a435e6219b32
Arturku <74879276+ArturkuB@users.noreply.github.com>
2025-02-09 00:00:53 +0100

    add HeartRateManager class

--- PulseApp/ContentView.swift
+++ PulseApp/ContentView.swift
@@ -1,8 +1,58 @@
 import SwiftUI
 import HealthKit
 
+class HeartRateManager: ObservableObject {
+    private let healthStore = HKHealthStore()
+    @Published var heartRate: Double = 0.0
+    
+    init() {
+        requestAuthorization()
+    }
+    
+    func requestAuthorization() {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            print("HealthKit unavailable")
+            return
+        }
+        let heartRateType = HKQuantityType.quantityType(forIdentifier: .heartRate)!
+        healthStore.requestAuthorization(toShare: [], read: [heartRateType]) { success, error in
+            if success {
+                self.startHeartRateQuery()
+            }
+            else {
+                print("error")
+            }
+        }
+    }
+    
+    func startHeartRateQuery() {
+        let heartRateType = HKQuantityType.quantityType(forIdentifier: .heartRate)!
+        let query = HKObserverQuery(sampleType: heartRateType, predicate: nil) {
+            _, completionHandler, _ in
+            self.fetchLatestHeartRate()
+            completionHandler()
+        }
+        healthStore.execute(query)
+    }
+    
+    func fetchLatestHeartRate() {
+        let heartRateType = HKQuantityType.quantityType(forIdentifier: .heartRate)!
+        let query = HKSampleQuery(sampleType: heartRateType, predicate: nil, limit: 1, sortDescriptors: nil) {
+            _, results, _ in
+            if let sample = results?.first as? HKQuantitySample {
+                DispatchQueue.main.async {
+                    self.heartRate = sample.quantity.doubleValue(for: HKUnit(from: "count/min"))
+                }
+            }
+        }
+        healthStore.execute(query)
+    }
+    
+}
 
 struct ContentView: View {
+    @StateObject private var heartRateManager = HeartRateManager()
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
