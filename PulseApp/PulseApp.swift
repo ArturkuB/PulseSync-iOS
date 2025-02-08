5f02ecea400853fba9d4f4aafdaa9caa41721ef8
Arturku <74879276+ArturkuB@users.noreply.github.com>
2025-02-08 23:56:25 +0100

    add project files

---
+++ PulseApp/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseAppApp.swift
+//  PulseApp
+//
+//  Created by Artur Balcer on 08/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
